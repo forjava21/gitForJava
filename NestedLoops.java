323809a8a451155e1338e6c0c00de1cbd373221d
forjava21 <83768931+forjava21@users.noreply.github.com>
2021-05-16 18:54:09 -0400

    Update NestedLoops.java

--- NestedLoops.java
+++ NestedLoops.java
@@ -4,12 +4,12 @@ import java.util.Date;
 public class NestedLoops {
 
 	public static void main(String[] args) {
-		//String str[][] = { { "red", "green" }, { "blue", "yellow" } };
-		//for (int i = 0; i < 2; i++) {
-		//	for (int j = 0; j < 2; j++) {
-		//		System.out.println(str[i][j]);
-		//	}
-		//}
+		String str[][] = { { "red", "green" }, { "blue", "yellow" } };
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.println(str[i][j]);
+			}
+		}
 		
 		//int[] array = new int[3];
 		//String[] s = new String[3];
@@ -19,16 +19,16 @@ public class NestedLoops {
 		//}
 		
 		//Date 
-		Date currentDate = new Date();
-		System.out.println(currentDate);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-		System.out.println(dateFormat.format(currentDate));
+		//Date currentDate = new Date();
+		//System.out.println(currentDate);
+		//SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		//System.out.println(dateFormat.format(currentDate));
 		
-		SimpleDateFormat dayOfTheweek = new SimpleDateFormat("EEEE");
-		System.out.println(dayOfTheweek.format(currentDate));
+		//SimpleDateFormat dayOfTheweek = new SimpleDateFormat("EEEE");
+		//System.out.println(dayOfTheweek.format(currentDate));
 		
-		SimpleDateFormat clockFormat = new SimpleDateFormat("h:mm a");
-		System.out.println(clockFormat.format(currentDate));
+		//SimpleDateFormat clockFormat = new SimpleDateFormat("h:mm a");
+		//System.out.println(clockFormat.format(currentDate));
 		
 		
 	}
